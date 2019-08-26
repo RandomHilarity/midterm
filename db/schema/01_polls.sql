347dc066373308f8b503cd4d7345688f64c3a4d5
John Webster <johnrwebs@gmail.com>
2019-08-26 23:13:35 +0000

    fixing links

--- db/schema/01_polls.sql
+++ db/schema/01_polls.sql
@@ -7,10 +7,7 @@ CREATE TABLE polls (
   created_at TIMESTAMP DEFAULT Now(),
   closes_at TIMESTAMP,
   comments_active BOOLEAN DEFAULT FALSE,
-  track_ip BOOLEAN DEFAULT FALSE,
-  track_voter_name BOOLEAN DEFAULT FALSE,
-  voters_can_change BOOLEAN DEFAULT FALSE,
-  votes_expected INTEGER DEFAULT 0
+  track_voter_name BOOLEAN DEFAULT FALSE
 );
 
 -- 5
--- db/schema/03_choices.sql
+++ db/schema/03_choices.sql
@@ -1,10 +1,11 @@
 DROP TABLE IF EXISTS choices CASCADE;
 CREATE TABLE choices (
   id SERIAL PRIMARY KEY NOT NULL,
+  question_id INTEGER REFERENCES questions(id) NOT NULL,
   title VARCHAR(255) NOT NULL,
   description TEXT,
-  question_id INTEGER REFERENCES questions(id) NOT NULL,
-  total_score INTEGER DEFAULT 0
+  times_answered INTEGER DEFAULT 0,
+  total_count INTEGER DEFAULT 0
 );
 
 -- 3
